cdf6028e1ecde08e1bf016288062fcbeab5dcccb
Brian Fohl <brian.fohl@gmail.com>
2024-11-03 18:18:28 -0800

    updates

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import logging
 
 app = Flask(__name__)
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
+# Set up a separate logger for user inputs
+user_input_logger = logging.getLogger('user_input_logger')
+user_input_logger.setLevel(logging.INFO)
+handler = logging.FileHandler('user_input.log')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+user_input_logger.addHandler(handler)
 
 @app.route('/')
 def home():
@@ -13,7 +19,7 @@ def home():
 @app.route('/submit', methods=['POST'])
 def submit():
     user_input = request.form['user_input']
-    app.logger.info(f'User input: {user_input}')
+    user_input_logger.info(user_input)
     return f'Thank you! You entered: {user_input}'
 
 if __name__ == '__main__':
