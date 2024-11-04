39d770a061ab91f714240025e847a19fc61089da
Brian Fohl <brian.fohl@gmail.com>
2024-11-03 16:04:07 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import logging
 
 app = Flask(__name__)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_input = request.form['user_input']
+    app.logger.info(f'User input: {user_input}')
+    return f'Thank you! You entered: {user_input}'
 
 if __name__ == '__main__':
     app.run(debug=True)
